3657023eae8ff032c0717d88f5eaf23b928b569f
iBlack14 <alonsoyhc@gmail.com>
2025-10-30 22:12:29 -0500

    SD

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@/contexts/ThemeContext';
+import { Toaster } from 'sonner';
+import MessageNotifier from '../components/MessageNotifier';
 import '../src/app/globals.css';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
@@ -8,6 +10,8 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
     <SessionProvider session={session}>
       <ThemeProvider>
         <Component {...pageProps} />
+        <Toaster position="bottom-right" richColors closeButton />
+        <MessageNotifier />
       </ThemeProvider>
     </SessionProvider>
   );
